699be8e8195df95811d620c8124b034f23fcebd4
Kazu Akiyama <kazunori.akiyama.kazu@gmail.com>
2020-01-22 18:33:10 -0500

    minor edits

--- difmap.rb
+++ difmap.rb
@@ -1,9 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Difmap < Formula
   desc "DIFMAP: A Caltech VLBI Program"
-  version "2.5e"
   homepage "http://www.astro.caltech.edu/~tjp/citvlb/index.html"
   url "ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5e.tar.gz"
   sha256 "457cd77c146e22b5332403c19b29485388a863ec494fff87137176396fc6a9ff"
@@ -13,10 +9,6 @@ class Difmap < Formula
   depends_on :x11
 
   fails_with :clang do
-    cause "Miscompilation resulting in segfault on queries"
-  end
-
-  fails_with :llvm do
     cause "Miscompilation resulting in segfault on queries"
   end
 
@@ -34,7 +26,7 @@ class Difmap < Formula
       s.change_make_var! "PGPLOT_LIB", pgplotlib
     end
 
-    system "./configure intel-osx-gcc"
+    system "./configure", "intel-osx-gcc"
     system "./makeall"
     
     prefix.install Dir['help/*.hlp']
@@ -43,17 +35,17 @@ class Difmap < Formula
 
   def caveats
     msg = <<~EOF
-Don't forget to add paths for PGPLOT before using DIFMAP.
-For instance, you can add the following lines to your ~/.bash_profile or ~/.zshrc file
-(and remember to source the file to update your current session):
+      Don't forget to add paths for PGPLOT before using DIFMAP.
+      For instance, you can add the following lines to your ~/.bash_profile or ~/.zshrc file
+      (and remember to source the file to update your current session):
 
-PGPLOT_DIR=`brew --prefix pgplot`/share
-if [ -e $PGPLOT_DIR ]; then
-  export PGPLOT_DIR=$PGPLOT_DIR
-  export PGPLOT_DEV=/xwin       # This is up to your preference. /xserve might be your choise.
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PGPLOT_DIR
-fi
-EOF
+      PGPLOT_DIR=`brew --prefix pgplot`/share
+      if [ -e $PGPLOT_DIR ]; then
+        export PGPLOT_DIR=$PGPLOT_DIR
+        export PGPLOT_DEV=/xwin       # This is up to your preference. /xserve might be your choise.
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PGPLOT_DIR
+      fi
+    EOF
   end
 end
 __END__
