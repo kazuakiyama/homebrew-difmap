aff3057d34701a173652630d84e03ed086f7da0d
Kazu Akiyama <kazunori.akiyama.kazu@gmail.com>
2020-01-22 19:05:10 -0500

    minor edits

--- difmap.rb
+++ difmap.rb
@@ -3,6 +3,7 @@ class Difmap < Formula
   homepage "http://www.astro.caltech.edu/~tjp/citvlb/index.html"
   url "ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5e.tar.gz"
   sha256 "457cd77c146e22b5332403c19b29485388a863ec494fff87137176396fc6a9ff"
+  version "2.5e"
 
   depends_on "gcc"
   depends_on "pgplot"
@@ -18,19 +19,19 @@ class Difmap < Formula
     ENV.fortran
     ENV.deparallelize
     ENV.append "CCOMPL", :gcc
-    
-    pgplotlib =  "-L#{HOMEBREW_PREFIX}/lib -lpgplot -lX11 -lpng"
 
-    inreplace 'configure' do |s|
+    pgplotlib = "-L#{HOMEBREW_PREFIX}/lib -lpgplot -lX11 -lpng"
+
+    inreplace "configure" do |s|
       s.change_make_var! "HELPDIR", prefix
       s.change_make_var! "PGPLOT_LIB", pgplotlib
     end
 
     system "./configure", "intel-osx-gcc"
     system "./makeall"
-    
-    prefix.install Dir['help/*.hlp']
-    bin.install ['difmap']
+
+    prefix.install Dir["help/*.hlp"]
+    bin.install ["difmap"]
   end
 
   def caveats
@@ -38,7 +39,6 @@ class Difmap < Formula
       Don't forget to add paths for PGPLOT before using DIFMAP.
       For instance, you can add the following lines to your ~/.bash_profile or ~/.zshrc file
       (and remember to source the file to update your current session):
-
       PGPLOT_DIR=`brew --prefix pgplot`/share
       if [ -e $PGPLOT_DIR ]; then
         export PGPLOT_DIR=$PGPLOT_DIR
@@ -46,6 +46,11 @@ class Difmap < Formula
         export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PGPLOT_DIR
       fi
     EOF
+    msg
+  end
+
+  test do
+    system "false"
   end
 end
 __END__
