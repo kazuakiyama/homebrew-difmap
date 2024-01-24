29f7601758212d4351ae8c15e39de7256b7ccbeb
Kazunori Akiyama <kakiyama@mit.edu>
2024-01-24 14:35:30 -0500

    update the formula for the latest DIFMAP version 2.5q

--- difmap.rb
+++ difmap.rb
@@ -1,10 +1,10 @@
 class Difmap < Formula
   desc "Caltech VLBI software package for interferometric imaging"
   homepage "https://sites.astro.caltech.edu/~tjp/citvlb/index.html"
-  url "ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5p.tar.gz"
-  version "2.5p"
-  sha256 "458fdc0f85eb03974b264061c0f171ad15e46d6f5d4fc45268f42950cbb3d8ea"
-  revision 1
+  url "ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5q.tar.gz"
+  version "2.5q"
+  sha256 "18f61641a56d41624e603bf64794c9f1b072eea320a0c1e0a22ac0ca4d3cef95"
+  revision 0
 
   depends_on "gcc"
   depends_on "libx11"
@@ -15,8 +15,8 @@ class Difmap < Formula
   end
 
   patch :p0 do
-    url "https://raw.githubusercontent.com/kazuakiyama/hb-difmap-patches/1a9d6fa75a6b27c87a10ac4f59abbf644349da8e/patch_difmap2.5p_configure.diff"
-    sha256 "1e906d857a06cce0a8066a489f032edafd5af34e1877b809688efe501d007482"
+    url "https://raw.githubusercontent.com/kazuakiyama/hb-difmap-patches/2496fc72884034f1daacf3959f8e73dc51c23a62/patch_difmap2.5q_configure.diff"
+    sha256 "49f48f93c2bdf294c7daa20448e3dd8914d67903847df74699f0ceafa23750d5"
   end
 
   def install
@@ -35,7 +35,14 @@ class Difmap < Formula
       inreplace "configure", "(cd libtecla_src; ./configure --without-man-pages)", "(cd libtecla_src ./configure --without-man-pages CFLAGS=-mmacosx-version-min=12.4.0)"
     end
 
-    system "./configure", "intel-osx-gcc"
+    on_intel do
+      system "./configure", "intel-osx-gcc"
+    end
+
+    on_arm do
+      system "./configure", "arm-osx-gcc"
+    end
+
     system "./makeall"
 
     prefix.install Dir["help/*.hlp"]
